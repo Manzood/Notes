9447e117f57a42fc566186063040c83192501fd5
Manzood <manzood.naqvi@gmail.com>
2020-06-11 19:49:59 +0530

    Updated the debugger template, Added some bit tricks and Insights.

---
+++ bit-tricks.cpp
@@ -0,0 +1,31 @@
+// obtaining the remainder of a number when divided by a power of two
+//considering S modulo N, where N -> power of two
+
+int rem=S^N;
+// example, S=7 (111) and N=4 (100), the remainder is 3 (011).
+
+
+// turn off the a particular bit in a number, say S
+s=s&(~(1<<n));
+// here, the nth bit will be turned off
+
+
+// toggle the nth bit of number S, while everything else stays off:
+s=s^(1<<n);
+
+// determine if a number is a power of two
+if (s & (s-1)==0) //then it is a power of two
+
+// turn off the last bit in a number 
+s & (s-1);
+// this is guaranteed to turn off the last bit
+
+// turn on the last zero in a number
+s |= s+1;
+// this will add one (perhaps a chain of additions of one), and thereby making sure that the next zero is turned on
+
+//turn off the last consecutive ones in a number
+s &= s+1;
+
+// turn on the last consecutive zeroes in a number
+s |= s-1;
--- debugger_template.cpp
+++ debugger_template.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 vector<string> vec_splitter(string s) {
