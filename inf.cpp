db24633da2155b7399c9546cf76620d0073c1a11
Manzood <manzood.naqvi@gmail.com>
2025-04-09 23:06:35 +0530

    Add inf

---
+++ inf.cpp
@@ -0,0 +1,9 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+
+// can be used for both int (where values are up to 10^9) also, despite the
+// overflow
+
+constexpr int INF = 0x3f3f3f3f3f3f3f3f;
